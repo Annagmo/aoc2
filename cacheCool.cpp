e282ed7b5c32b3f489d969f406574b3c23b0b408
Annagmo <85114312+Annagmo@users.noreply.github.com>
2022-11-10 22:23:56 -0300

    Update cacheCool.cpp

--- cacheCool.cpp
+++ cacheCool.cpp
@@ -5,14 +5,15 @@
 #include <stdint.h>
 #include <time.h>
 
-int substitui(char *sub, int assoc);
+int substitui(char *sub, int bits_val [],int n_bits_indice);
+
 static void Help(){
-	printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+	printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
 	printf("\t\tBem-vindo à cacheCool:\n");
 	printf(" Você digitou um número incorreto de argumentos. Utilize:\n");
 	printf("./cacheCool <nsets> <bsize> <assoc> <substituição> <flag_saida> arquivo_de_entrada\n");
 	printf("\n  Ex.: ./cacheCool 64 8 1 R 0 bin_100.bin\n");
-	printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+	printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
 	exit(EXIT_FAILURE);
 }
 
@@ -28,7 +29,14 @@ int main( int argc, char *argv[ ] )
 	int flagOut = atoi(argv[5]);
 	char *arquivoEntrada = argv[6];
 
-		int miss_compulsorio =0, n_bits_tag =0;
+	//printf("nsets = %d\n", nsets);
+	//printf("bsize = %d\n", bsize);
+	//printf("assoc = %d\n", assoc);
+	//printf("subst = %s\n", subst);
+	//printf("flagOut = %d\n", flagOut);
+	//printf("arquivo = %s\n", arquivoEntrada);
+
+		int miss_compulsorio =0, n_bits_tag =0, Ncheio = 0, vazio =0;
 		int endereco = 0, tag =0, indice = 0, miss =0, hit =0, i =0, miss_conflito =0, miss_capacidade=0, qtd_acessos=0;
 		float taxa_hit=0.0, taxa_miss=0.0, taxa_miss_comp=0.0,taxa_miss_conf=0.0, taxa_miss_cap=0.0;
 		int n_bits_offset =0, n_bits_indice =0;
@@ -37,31 +45,32 @@ int main( int argc, char *argv[ ] )
 		n_bits_offset = (log2(bsize));
 		n_bits_indice = (log2(nsets));
 		n_bits_tag = 32 - n_bits_offset - n_bits_indice;
+		//descobre o número de bits de cada parcela do endereço para todos os endereços do arquivo
+
 		//----------------------------------------------------------
 		//Variaveis para o conjunto associativo;
+		int achou = 0;
 		typedef struct{
 			int val;
 			int tag;
 			int info;
-		}Cache;
-		Cache cache[nsets * assoc][assoc] = {0};
-		int CA_val_assoc[assoc] = {0};
+		}Cache; //cria uma struct de cache pq vai ter uma posicao em cada conjunto para o mesmo indice
+		Cache cache[nsets * assoc][assoc] = {0}; //inicializa tudo em 0
+
 		//----------------------------------------------------------
 		//variaveis p/ o arquivo:
 		FILE *fptr;
 		fptr = fopen(arquivoEntrada, "rb"); //rb  para arquivos binarios.
 		//----------------------------------------------------------
-		//variveis apos apresentaçao:
-		int qntdLinhas = 0;
+		int CA_val_assoc[nsets * assoc]={0};
 		int verificaCap = 0;
 
 		if(fptr == NULL) {
 			printf("Cannot open file \n");
-			exit(0); //se não conseguir abrir o arquivo
+			exit(0); //se não puder abrir o arquivo
 		}
 		while (!feof(fptr))
 		{
-
 			//para ler o arq binario:
 			unsigned char buffer[4]; 
 			fread(buffer,sizeof(int),1,fptr);
@@ -73,14 +82,16 @@ int main( int argc, char *argv[ ] )
 			tag = endereco >> ( n_bits_offset + n_bits_indice );
 			indice = indice >> n_bits_offset;
 			indice = ( endereco & mask );
+			indice = indice >> n_bits_offset;
 
 			if(assoc == 1){ //Mapeamento Direto
 			if (cache_val[indice] == 0)
 			{
-				miss++; 		
+				miss++; 		//nao tava contando os miss_compulsorio pro tot de misses.
 				miss_compulsorio++;
 				cache_val[indice] = 1;
 				cache_tag[indice] = tag;
+				// estas duas últimas instruções representam o tratamento da falta.
 			}
 			else
 				if (cache_tag[indice] == tag)
@@ -90,21 +101,21 @@ int main( int argc, char *argv[ ] )
 					miss++;
 					
 					// conflito ou capacidade?
-					while(qntdLinhas != (nsets*assoc)){          //coloquei um contador ao inves do for p/ percorrer cache
-						if(cache_val[qntdLinhas] == 0){
+					for(i=0; i < sizeof(cache_val); i++){
+						if(cache_val[i] == 0){
 							miss_conflito++;
 							cache_val[indice] = 1;
 							cache_tag[indice] = tag;
 						}
 						else{miss_capacidade++;
+							indice = substitui(subst, cache_val,n_bits_indice);
 							cache_val[indice] = 1;
 							cache_tag[indice] = tag;
-						}
-						qntdLinhas++;
+							}
 					}
 				}
 			} 
-			if (assoc > 1) { //Conjunto Associativo
+			if (assoc != 1) { //Conjunto Associativo
 				for(i=0; i<assoc; i++){
 					if(cache[indice][i].info == endereco &&  cache[indice][i].tag == tag && cache[indice][i].val == 1 ){ //i representa o conjunto naquela posiçao. Ex.: Assoc = 2 temos cache[indice][0] e cache[indice][1]
 						hit++;
@@ -149,34 +160,35 @@ int main( int argc, char *argv[ ] )
 				}
 			}
 		}
-		taxa_hit = (((float)hit)/((float)qtd_acessos-1));
-		taxa_miss = (((float)miss)/((float)qtd_acessos-1));
-		taxa_miss_cap = (((float)miss_capacidade)/((float)qtd_acessos-1));
-		taxa_miss_comp = (((float)miss_compulsorio)/((float)qtd_acessos-1));
-		taxa_miss_conf = (((float)miss_conflito)/((float)qtd_acessos-1));
+		qtd_acessos = qtd_acessos-1;//Lembrando que, o -1 pq o acesso do EOF tambem eh 1 acesso
+		taxa_hit = (((float)hit/qtd_acessos)*100);
+		taxa_miss = (((float)miss/qtd_acessos)*100);
+		taxa_miss_cap = (((float)miss_capacidade/qtd_acessos)*100);
+		taxa_miss_comp = (((float)miss_compulsorio/qtd_acessos)*100);
+		taxa_miss_conf = (((float)miss_conflito/qtd_acessos)*100);
 
 		if(flagOut==0){
-			printf("\n-------------------------------\n");
-			printf("Qntd Acessos: %d\n",qtd_acessos-1);
+			printf("-------------------------------\n");
+			printf("Qntd Acessos: %d\n",qtd_acessos); 
 			printf("hit: %d\n",hit);
 			printf("miss: %d\n",miss);
-			printf("Taxa de hit: %.5f\n",taxa_hit);
-			printf("Taxa de miss: %.5f\n",taxa_miss);
-			printf("Taxa de miss de compulsório: %.5f\n",taxa_miss_comp);
-			printf("Taxa de miss de capacidade: %.5f\n",taxa_miss_cap);
-			printf("Taxa de miss de conflito: %.5f\n",taxa_miss_conf);
+			printf("-------------------------------\n");
+			printf("Taxa de hit: %.3f\n",taxa_hit);
+			printf("Taxa de miss: %.3f\n",taxa_miss);
+			printf("Taxa de miss de compulsório: %.3f\n",taxa_miss_comp);
+			printf("Taxa de miss de capacidade: %.3f\n",taxa_miss_cap);
+			printf("Taxa de miss de conflito: %.3f\n",taxa_miss_conf);
 			printf("-------------------------------\n");
 		}
 		else if (flagOut == 1){
-			printf("%d, %.2f, %.2f, %.2f, %.2f, %.2f\n",qtd_acessos-1, taxa_hit, taxa_miss, taxa_miss_comp, taxa_miss_cap, taxa_miss_conf);
+			printf("%d, %.3f, %.3f, %.3f, %.3f, %.3f\n",qtd_acessos, taxa_hit/100, taxa_miss/100, taxa_miss_comp/100, taxa_miss_cap/100, taxa_miss_conf/100);
 		}
 		fclose(fptr);
 		
 	return 0;
 }
-
-int substitui(char *sub, int assoc) {
-	if(*sub == 'r' || *sub == 'R'){
-		return (rand() % assoc);  
+int substitui(char *sub, int bits_val [],int n_bits_indice) {
+	if(*sub == 'r'){
+		return 1 + (rand() % sizeof(n_bits_indice));		// gera aleatório entre 1 e a quantidade total de índice   
 	}
 }
